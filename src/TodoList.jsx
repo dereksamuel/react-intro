acb41e5346bfd4c003bfee8f4e761ea09d2eddc7
Pipaplutarco <dereksamuelgr@gmail.com>
2021-11-08 22:11:07 -0500

    all for today

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,6 +1,19 @@
+/* eslint-disable no-undef */
+import { useEffect } from "react";
 import "./TodoList.css";
 
 function TodoList (props) {
+  useEffect(() => {
+    let elem = document.querySelector(".TodoList");
+    let msnry = new Masonry(elem, {
+      // options
+      itemSelector: ".TodoItem",
+      columnWidth: 400,
+      isFitWidth: true,
+      gutter: 20,
+    });
+  }, []);
+
   return (
     <ul className="TodoList">
       { props.children }
